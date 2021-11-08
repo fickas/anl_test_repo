efe169a55c91493c4a7039a5c2cf36c0bc4f6f81
fickas <fickas@cs.uoregon.edu>
2021-11-08 15:36:05 -0800

    Update arithmetic.py

--- folder1/arithmetic.py
+++ folder1/arithmetic.py
@@ -1,18 +1,51 @@
-## @package anl_test_repo
-#  Attempting to mock portions of Flash project.
 
-##add function
-#Adds two numbers.
-#  @param x a number.
-#  @param y a number.
-def add(x,y):
-  return x+y
+def sub(x,y,round=None):
+  """
+  Return the subtraction of two numbers::
 
-##sub function
-#Subtracts two numbers.
-#  @param x a number.
-#  @param y a number.
-def sub(x,y):
+  .. math:: x - y = z
+
+  Parameters
+  ----------
+  x: int, real, complex
+     first operand
+  y: int, real, complex
+     second operand
+  round: positive int, optional
+     If None, does no rounding. Else rounds the result to places specified, e.g., 2.
+
+  Returns
+  -------
+  z: int, real, complex
+      the result of subtraction
+
+  Raises
+  ------
+  ValueError:
+      when 'x' or 'y' is not a number
+
+  See Also
+  --------
+  Of course, subtraction is built-in to Python as minus sign.
+
+  Notes
+  -----
+  The algorithm is a straightforward implementation of
+  subtraction.
+
+  References
+  ----------
+  .. [1] Any textbook on mathematics.
+
+  Examples
+  --------
+  >>> sub(1,2)
+  -1
+  >>> sum(.03333, .01111)
+  .02222
+  >>> sum(.03333, .01111, round=3)
+  .022
+  """
   return x-y
 
 ##list_add function
