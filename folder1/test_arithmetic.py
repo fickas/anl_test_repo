bc4e232d01b3fb9dee64b07525f90779dbda8774
fickas <fickas@cs.uoregon.edu>
2021-11-15 09:57:07 -0800

    Update test_arithmetic.py

--- folder1/test_arithmetic.py
+++ folder1/test_arithmetic.py
@@ -1,7 +1,7 @@
 from arithmetic import sub
 
-  def test_sub():
-    assert sub(2,1)==1
-    assert sub(.3333, .1111)==.2222
-    assert sub(.3333, .1111, round=2)==.22
+def test_sub():
+  assert sub(2,1)==1
+  assert sub(.3333, .1111)==.2222
+  assert sub(.3333, .1111, round=2)==.22
     
