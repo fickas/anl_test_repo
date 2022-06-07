8391a9b922fd79c0a977e3ec3c61f7c03d374c25
Juan-Pablo-Flores <jpfloresd.97@gmail.com>
2022-06-07 08:30:17 -0700

    Add tests for prod function

--- folder1/test_arithmetic.py
+++ folder1/test_arithmetic.py
@@ -1,4 +1,4 @@
-from arithmetic import sub
+from arithmetic import sub, prod
 
 #Adding this comment to just test committing and diff data
 def test_sub():
@@ -6,4 +6,9 @@ def test_sub():
   assert sub(4,2)==2
   assert sub(.3333, .1111)==.2222
   assert sub(.3333, .1111, round=2)==.22
+
+def test_prod():
+  assert prod([])==1
+  assert prod([1,2,0])==0
+  assert prod([.03333, .01111]) - 0.0003702963 < 1e-10
     
