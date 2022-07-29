f7eebda5a99dd75d15f819b61b7b9bddeab04e75
Stephen Fickas <fickas@cs.uoregon.edu>
2022-07-29 15:16:26 -0700

    Update test_arithmetic.py

--- folder1/test_arithmetic.py
+++ folder1/test_arithmetic.py
@@ -1,9 +1,9 @@
 from arithmetic import sub
 
-#Adding this comment to just test committing and diff data
+#Unit tests for sub function
 def test_sub():
   assert sub(2,1)==1
   assert sub(4,2)==2
-  #assert sub(.3333, .1111, round=4)==.2222
-  #assert sub(.3333, .1111, round=2)==.22
+  assert sub(.3333, .1111, round=4)==.2222
+  assert sub(.3333, .1111, round=2)==.22
     
