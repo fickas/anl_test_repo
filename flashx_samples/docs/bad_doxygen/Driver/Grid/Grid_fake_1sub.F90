d37f0609a7b4f77fe4b3979b1d84b25db109cab6
Ellie Kobak <elliekobak@Ellies-MacBook-Air.local>
2023-06-21 11:07:08 -0700

    changes 2,3,11

--- flashx_samples/docs/bad_doxygen/Driver/Grid/Grid_fake_1sub.F90
+++ flashx_samples/docs/bad_doxygen/Driver/Grid/Grid_fake_1sub.F90
@@ -15,21 +15,21 @@
 
 ! FORMATTING RULES
 !
-! Analogous to those for the MyUnit_myRoutine stub.
+! Analogous to those for the Grid_myRoutine stub.
 
-!> @ingroup MyUnit
-!! @anchor MyUnit_simpleGeneric_int_stub
+!> @ingroup Grid
+!! @anchor Grid_fake_1sub_stub
 !!
-!! @brief Integer version of @ref myunit_interface::myunit_simplegeneric "MyUnit_simpleGeneric generic interface"
+!! @brief Integer version of @ref grid_interface::grid_fake_1sub "Grid_fake_1sub generic interface"
 !! 
 !! @details
 !! This section should be written following the same rules for the details
-!! section of the MyUnit_myRoutine stub and should establish the contract for
+!! section of the Grid_myRoutine stub and should establish the contract for
 !! all versions of this generic interface.
 !!
 !! @param a   My a argument
 !! @param b   My b argument
-subroutine MyUnit_simpleGeneric_int(a, b)
+subroutine Grid_fake_1sub_int(a, b)
     implicit none
 
     ! NOTE: The full argument details are *not* included in the @param above
@@ -38,21 +38,4 @@ subroutine MyUnit_simpleGeneric_int(a, b)
     real,    intent(OUT) :: b
 
     ...
-end subroutine MyUnit_simpleGeneric_int
-
-!> @ingroup MyUnit
-!! @anchor MyUnit_simpleGeneric_real_stub
-!!
-!! @brief Real version of @ref myunit_interface::myunit_simplegeneric "MyUnit_simpleGeneric generic interface"
-!! 
-!! @details
-!! This version is a trivial variant of the Integer version.  Refer to the
-!! documentation of MyUnit_simpleGeneric_int for more information.
-subroutine MyUnit_simpleGeneric_real(a, b)
-    implicit none
-
-    real, intent(IN)  :: a
-    real, intent(OUT) :: b
-
-    ...
-end subroutine MyUnit_simpleGeneric_real
+end subroutine Grid_fake_1sub_int
