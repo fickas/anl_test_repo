346ca5d4d677d51b841313ec5c5d745421f47dbd
Ellie Kobak <elliekobak@Ellies-MacBook-Air.local>
2023-07-07 15:28:50 -0700

    local api changes

--- flashx_samples/docs/bad_doxygen/MyUnit/localApi/mu_myRoutine.F90
+++ flashx_samples/docs/bad_doxygen/MyUnit/localApi/mu_myRoutine.F90
@@ -17,9 +17,7 @@
 !
 ! Analogous to those for the MyUnit_myRoutine stub.
 
-!> @ingroup MyUnitPrivate
-!! @anchor mu_myRoutine_stub
-!!
+!> 
 !! @brief <Add in one-line description>
 !! 
 !! @details
@@ -27,8 +25,6 @@
 !! section of the MyUnit_myRoutine stub.
 !!
 !! @param a   My a argument
-!! @param b   My b argument
-!! @param c   My c argument
 subroutine mu_myRoutine(a, b, c)
     implicit none
 
